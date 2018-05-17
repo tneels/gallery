1596c42c89fcb0f12dcca0e1e5324e028c231ee1
Thomas Neels <thomas.neels@studio.plus>
2018-05-17 13:55:45 +0200

    slick slider integration

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,10 +2,29 @@
 
 
 // SLICK SLIDER
-$('.slick-slider').slick({
+// $('.slick-slider').slick({
+// 	dots: true,
+// 	infinite: true,
+// 	speed: 300
+// 	// prevArrow: '<span class="icon-arrow-left"></span>',
+// 	// nextArrow: '<span class="icon-arrow-right"></span>'
+// });
+
+
+$('.slick-for').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	asNavFor: '.slick-nav'
+});
+
+$('.slick-nav').slick({
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	asNavFor: '.slick-for',
 	dots: true,
-	infinite: true,
-	speed: 300,
-	prevArrow: '<span class="icon-arrow-left"></span>',
-	nextArrow: '<span class="icon-arrow-right"></span>'
+	centerMode: true,
+	focusOnSelect: true,
+	arrows: false,
+	fade: true
 });
